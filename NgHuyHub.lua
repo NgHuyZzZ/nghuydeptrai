d5107474297a28fae090d54b659a0c96c0276317
Nguyen Minh Huy <145020294+NgHuyZzZ@users.noreply.github.com>
2024-02-02 10:06:07 +0700

    Update NgHuyHub.lua

--- NgHuyHub.lua
+++ NgHuyHub.lua
@@ -1,2 +1,7 @@
 if game.PlaceId == 7449423635 or game.PlaceId == 2753915549 or game.PlaceId == 4442272183 then
       loadstring(game:HttpGet("https://raw.githubusercontent.com/NgHuyZzZ/nghuydeptrai/main/BloxFruits.lua"))()
+elseif game.PlaceId == 13772394625 or game.PlaceId == 14915220621 or game.PlaceId == 14732610803 or game.PlaceId ==  15264892126 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NgHuyZzZ/nghuydeptrai/main/BladeBall.lua"))()
+else
+    print("Not Support")
+end
