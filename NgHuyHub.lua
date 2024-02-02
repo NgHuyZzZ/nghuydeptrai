0966caabfd3757acd9e9ca898636a2f237f350b4
Nguyen Minh Huy <145020294+NgHuyZzZ@users.noreply.github.com>
2024-02-02 10:01:20 +0700

    Update NgHuyHub.lua

--- NgHuyHub.lua
+++ NgHuyHub.lua
@@ -1,2 +1,2 @@
-if game.PlaceId == 155615604 then
+if game.PlaceId == 7449423635 or game.PlaceId == 2753915549 or game.PlaceId == 4442272183 then
       loadstring(game:HttpGet("https://raw.githubusercontent.com/NgHuyZzZ/nghuydeptrai/main/BloxFruits.lua"))()
