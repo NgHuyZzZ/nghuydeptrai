97183755dad31f6b5fe1127eb3c3d309aaa383ea
Nguyen Minh Huy <145020294+NgHuyZzZ@users.noreply.github.com>
2024-02-02 10:44:36 +0700

    Add files via upload

---
+++ BladeBall.lua
@@ -0,0 +1,11 @@
+shared.config = {
+   adjustment = 3.6, -- // Keep this between 3 to 4 \\ --
+   hit_range = 0.5, -- // You can mess around with this \\ --
+
+   mode = 'Always', -- // Hold , Toggle , Always \\ --
+   deflect_type = 'Remote', -- // Key Press , Remote \\ --
+   notifications = true,
+   keybind = Enum.KeyCode.E
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NgHuyZzZ/nghuydeptrai/main/BladeBallMain.lua"))()
