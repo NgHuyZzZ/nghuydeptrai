92c23bb62fdc97d83c75e66fbe8253cbbc268cba
Nguyen Minh Huy <145020294+NgHuyZzZ@users.noreply.github.com>
2024-02-29 10:48:46 +0700

    Update NgHuyHub.lua

--- NgHuyHub.lua
+++ NgHuyHub.lua
@@ -1,6 +1,12 @@
-if game.PlaceId == 7449423635 or game.PlaceId == 2753915549 or game.PlaceId == 4442272183 then
-      loadstring(game:HttpGet("https://raw.githubusercontent.com/NgHuyZzZ/nghuydeptrai/main/gg.lua"))()
-elseif game.PlaceId == 13772394625 or game.PlaceId == 14915220621 or game.PlaceId == 14732610803 or game.PlaceId ==  15264892126 then
+repeat task.wait() until game:IsLoaded()
+local TablePlace = {7449423635,2753915549,4442272183}
+if table.find(TablePlace,game.PlaceId) then 
+if getgenv().OldHub then 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NgHuyZzZ/nghuydeptrai/main/BloxFruits.lua"))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NgHuyZzZ/nghuydeptrai/main/gg.lua"))()
+end
+if game.PlaceId == 13772394625 or game.PlaceId == 14915220621 or game.PlaceId == 14732610803 or game.PlaceId ==  15264892126 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/NgHuyZzZ/nghuydeptrai/main/BladeBall.lua"))()
 elseif game.PlaceId == 3101667897 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/NgHuyZzZ/nghuydeptrai/main/LOS.lua"))()
